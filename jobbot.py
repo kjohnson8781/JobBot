5762e7d9e376f6cd176644bb7f87a9576a5609fa
Kara <kjj8781@gmail.com>
2022-08-14 20:48:32 -0500

    get url working

--- jobbot.py
+++ jobbot.py
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup as bs
 starttime = time.time()
 driver = wb.Firefox()
 
-descrip = ['PlayStation', 'Python', 'SQL']
+descrip = ['Hardware', 'Python', 'Server']
 urls = []
 
 location = 'Austin'
@@ -26,7 +26,7 @@ joblist = driver.find_element(By.CSS_SELECTOR, "ul[class^='jobsearch-ResultsList
 # driver.find_element(By.CLASS_NAME, "jobsearch-ResultsList css-0") 
 # /html/body/div/div[2]/div/span/div[4]/div[5]/div[2]/div/div/div[2]/div[1]/ul
 jobads = joblist.find_elements(By.CSS_SELECTOR, "div[class^='cardOutline tapItem']")
-for i in range(len(jobads)-1,len(jobads)):
+for i in range(len(jobads)):
     # print("-----------------------------------")
     # print(jobads[i].text)
 
@@ -64,35 +64,19 @@ for i in range(len(jobads)-1,len(jobads)):
 
 
             job_finder = soup.find('div', class_='jobsearch-jobDescriptionText')
-            print(job_finder)
-            all_text = job_finder.find_all('br')
-            # for text in all_text:
+            # print(job_finder)
+            text = job_finder.get_text()
+            # all_text = job_finder.find_all('br')
+            # for text in job_finder:
+            #     print(text.get_text())
+            for j in range(len(descrip)):
+                #     print("descrip j", descrip[j], ", text, ", text.get_text())
+                if(str(descrip[j]) in text):
+                    url = driver.current_url
+                    urls.append(url)
+            print(urls)
+                    # print("FOUND KEYWORD")
                 
-
-            # companytext = company_finder.get_text()
-            # print(companytext)
-            # time.sleep(.5)
-
-            # key_finder = soup.find_all('div', class_='jobsearch-jobDescriptionText')
-            # # print('len', len(key_finder))
-            # for key in key_finder:
-            #     ptags = key.find_all('p')
-            #     for text in ptags:
-            #         # print("d len", len(descrip))
-            #         for j in range(len(descrip)):
-            #             # print("descrip j", descrip[j], ", text, ", text.get_text())
-            #             if(str(descrip[j]) in text.get_text()):
-            #                 found_key = True
-                    
-            
-            # if(found_key):
-            #     url = driver.current_url
-            #     urls.append(url)
-            
-            # print(urls)
-            
-
-            # print(soup)
             driver.switch_to.default_content()
             time.sleep(1)
 
@@ -105,10 +89,12 @@ for i in range(len(jobads)-1,len(jobads)):
 
 time.sleep(100)
 driver.quit()
+
+
 # print(time.time() - starttime)
 # try:
 #     test = driver.find_element(By.XPATH, '')
 
-# driver.find_element_by_xpath('//*[@id="root"]/div[2]/div[2]/div/form/div[2]input').send_keys("kjj8781@tamu.edu")
-# driver.find_element_by_xpath('//*[@id="root"]/div[2]/div[2]/div/form/div[3]/input').send_keys("Schoo!2022")
+# driver.find_element_by_xpath('//*[@id="root"]/div[2]/div[2]/div/form/div[2]input').send_keys("USER")
+# driver.find_element_by_xpath('//*[@id="root"]/div[2]/div[2]/div/form/div[3]/input').send_keys("PASS")
 # time.sleep(5)
